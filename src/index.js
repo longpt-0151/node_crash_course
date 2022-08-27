044988527dd726fb3066e61221134beacaae2744
Pham Thanh Long <pham.thanh.long@sun-asterisk.com>
2022-08-27 16:18:59 +0700

    controller and routes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,16 @@ const morgan = require('morgan')
 const app = express()
 const port = 3000
 
+const route = require('./routes')
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+// XMLHttpRequest, fetch, axios
 
 //http logger
 app.use(morgan('tiny'))
@@ -18,14 +27,7 @@ app.engine('hbs', engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'))
 
-
-app.get('/', (req, res) => {
-res.render('home');
-});
-
-app.get('/news', (req, res) => {
-res.render('news');
-});
+route(app);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
